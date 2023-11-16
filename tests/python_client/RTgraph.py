c109d6dd1bb2d80af1d732c8f19ec62a7fca65f5
ELopes <overewer@gmail.com>
2023-11-16 19:41:03 -0300

    RT client

--- tests/python_client/RTgraph.py
+++ tests/python_client/RTgraph.py
@@ -1,32 +1,59 @@
+import http.client
+import time
 import matplotlib.pyplot as plt
-import numpy as np
-from itertools import count
+import csv
 from matplotlib.animation import FuncAnimation
+from requests.exceptions import ConnectionError
 
-# Initialize empty lists to store data points
 x_data = []
-y_data = []
+t_data = []
 
-# Create a function to add new data points
+starttime = time.time()
+
+conn = http.client.HTTPConnection("192.168.4.1:80")
+endpoint = "/"
+csv_filename = "data/Graphs.csv"
+
 def add_data():
-    x_data.append(next(index))
-    y_data.append(np.random.randint(0, 100))  # Replace this with your actual data source
+    global conn
+    try:
+        conn.request("GET", endpoint)
+        r1 = conn.getresponse()
+        while chunk := r1.read(200):
+            output = float(chunk.decode())
+        
+        actualtime = time.time() - starttime
 
-# Create a counter for the x-axis
-index = count()
+        x_data.append(output)
+        t_data.append(actualtime)
 
-# Create a function to update the graph
+        with open(csv_filename, mode='a', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow([actualtime, output])
+
+        conn.close()
+    except ConnectionRefusedError as e:
+        print(f"Connection error: {e}")     
+
+
 def update_graph(i):
     add_data()
     plt.cla()  # Clear the previous plot
-    plt.plot(x_data, y_data, label='Data')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.legend()
 
-# Create an animated plot
-ani = FuncAnimation(plt.gcf(), update_graph, interval=1000)  # Update every 1 second (1000 milliseconds)
+    # Customize the plot aesthetics
+    plt.plot(t_data, x_data, label='Temperature', color='b', linewidth=0, marker='o', markersize=5)
+    plt.xlabel('Time')
+    plt.ylabel('Temperature')
+    plt.title('Temperature Caracterization',fontsize=16)
+    plt.grid(True, linestyle='--', alpha=0.5)
+    plt.legend(loc='upper left')
+    
+    # Beautify the x-axis timestamps (optional)
+    plt.gcf().autofmt_xdate()
 
-# Show the plot
+    plt.gca().set_facecolor('#f5f5f5')  # Set background color
+
+ani = FuncAnimation(plt.gcf(), update_graph, frames=None,repeat=False, interval=1000)  # Update every 1 second (1000 milliseconds)
+
 plt.tight_layout()
 plt.show()
