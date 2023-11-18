ce57bb0315b1dfa409e416c16a19d48dde0dacbc
ELopes <overewer@gmail.com>
2023-11-18 16:46:53 -0300

    Testing PID

--- tests/TestePID/main.c
+++ tests/TestePID/main.c
@@ -12,28 +12,31 @@ void delay(unsigned int milliseconds){
 }
 
 float in,out,set,amb = 0.0f;
+int temp = 0;
 upid mainpid;
 
 int main()
 {
     srand(time(NULL));
-    pid_create(&mainpid,&in,&out,&set,6,0.02,0,512,0);
+    pid_create(&mainpid,&in,&out,&set,2.1,0.048,1.92,512,0);
 
     amb = 25.0f;
     float pld[10] = {};
     int pldi = 0;
     out = 0.0f;
-    set = 50.0f;
+    set = 150.0f;
+    temp = 0;
     
     while(1)
     {
         pid_run(&mainpid);
         pld[pldi++] = out;
         in = amb + (((float)rand() / RAND_MAX) - 0.5);
-        amb += 0.008*pld[9]-0.001f*(in-25.0f);
+        amb += (0.01196*(pld[9]/512))*temp++ - 0.001f*(in-25.0f);
         if(pldi > 9) pldi = 0;        
         printf("IN: %f\nOUT: %f\nSETPOINT: %f\n\n", in,out,set);
-        delay(1000);
+
+        delay(200);
     }
 
     return 0;
